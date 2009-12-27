3612cf570f0d0364be3c63223e3001df4361baaa
Javier Goday <jgoday@gmail.com>
2009-12-27 17:16:40 +0100

    Sample add/edit/delete items in adminitems snippet

--- src/main/scala/bootstrap/liftweb/Boot.scala
+++ src/main/scala/bootstrap/liftweb/Boot.scala
@@ -1,24 +1,59 @@
 package bootstrap.liftweb
 
+import java.sql.{Connection, DriverManager}
+
 import _root_.net.liftweb.common._
 import _root_.net.liftweb.util._
 import _root_.net.liftweb.http._
+import _root_.net.liftweb.mapper.{DB, ConnectionIdentifier, ConnectionManager, DefaultConnectionIdentifier, Schemifier}
 import _root_.net.liftweb.sitemap._
 import _root_.net.liftweb.sitemap.Loc._
 import Helpers._
+
+import com.sample.model.Item
 
 /**
   * A class that's instantiated early and run.  It allows the application
   * to modify lift's environment
   */
 class Boot {
-  def boot {
-    // where to search snippet
-    LiftRules.addToPackages("com.sample")
+    def boot {
+        DB.defineConnectionManager(DefaultConnectionIdentifier, DefaultDBVendor)
+        Schemifier.schemify(true, Log.infoF _, Item)
 
-    // Build SiteMap
-    val entries = Menu(Loc("Home", List("index"), "Home")) :: Nil
-    LiftRules.setSiteMap(SiteMap(entries:_*))
+        // where to search snippet
+        LiftRules.addToPackages("com.sample")
+
+        // List items menu
+        val itemsMenu = Menu(Loc("Items", "items" :: "index" :: Nil, "List items"))
+ 
+        // Build SiteMap
+        val entries = Menu(Loc("Home", List("index"), "Home")) :: itemsMenu :: Nil
+
+
+        LiftRules.setSiteMap(SiteMap(entries:_*))
   }
+}
+
+object DefaultDBVendor extends ConnectionManager {
+    def newConnection(name: ConnectionIdentifier): Box[Connection] = {
+        try {
+            val connectionUrl = Props.get("db.url").openOr("") + "?user=" +
+                                Props.get("db.username").openOr("") + "&password=" +
+                                Props.get("db.password").openOr("")
+
+            Class.forName(Props.get("db.driver").open_!)
+            val dm = DriverManager.getConnection(connectionUrl)
+            return Full(dm)
+        }
+        catch {
+            case e: Exception => {
+                Log.error(e.getMessage);
+                return Empty
+            }
+        }
+    }
+
+    def releaseConnection(conn: Connection) = conn.close
 }
 
---
+++ src/main/scala/com/sample/snippet/AdminItems.scala
@@ -0,0 +1,127 @@
+package com.sample.snippet
+
+import scala.xml.{NodeSeq, Text}
+import net.liftweb.common.{Box, Full}
+import net.liftweb.http.{RequestVar, S, SHtml, TemplateFinder}
+import net.liftweb.util.{Helpers, Log}
+import Helpers._
+
+import com.sample.model.Item
+
+
+class AdminItems {
+    private val ITEM_TEMPLATE = "templates-hidden/item_form"
+
+    private object ShowAddItem  extends RequestVar[Int](0)
+    private object SelectedItem extends RequestVar[Long](0)
+
+    def clear = {
+        ShowAddItem(0)
+        SelectedItem(0)
+    }
+
+    def showAddItem(node: NodeSeq): NodeSeq = {
+        ShowAddItem.get match {
+            case 1 => {
+                var name = ""
+                val template: NodeSeq = TemplateFinder.findAnyTemplate(ITEM_TEMPLATE :: Nil).openOr(<p></p>)
+                val content = bind("itemForm", template, "title"  -> Text("New item"),
+                                                          "name"   -> SHtml.text(name, name = _),
+                                                          "id"     -> Text(""),
+                                                          "submit" -> SHtml.submit("save", () => addItem(name)),
+                                                          "close"  -> SHtml.link("index", () => clear, Text("close")))
+
+                <div>{content}</div>
+            }
+            case _ => SHtml.link("index", () => ShowAddItem(1), <p>Crear</p>)
+        }
+    }
+
+    def showEditItem(node: NodeSeq): NodeSeq = {
+        if (SelectedItem.get > 0) {
+            var id = SelectedItem.get
+            val item = Item.find(id).open_!
+
+            var name = item.name.is
+
+            val template = TemplateFinder.findAnyTemplate(ITEM_TEMPLATE :: Nil).openOr(<p></p>)
+            val content  = bind("itemForm", template, "title"  -> Text("Edit item"),
+                                                      "name"   -> SHtml.text(name, name = _),
+                                                      "id"     -> SHtml.hidden(() => {id = id}),
+                                                      "submit" -> SHtml.submit("save", () => saveItem(id, name)),
+                                                      "close"  -> SHtml.link("index", () => clear, Text("close")))
+
+            <div>{content}</div>
+        }
+        else {
+            <div></div>
+        }
+    }
+
+    def list(node: NodeSeq): NodeSeq = {
+        Item.findAll match {
+            case Nil   => Text("There is no items in database")
+            case items => items.flatMap(i => bind("item", node, "name"   -> {i.name},
+                                                                "edit"   -> getEditLink(i.id),
+                                                                "remove" -> getRemoveLink(i.id)))
+        }
+    }
+
+
+    private def addItem(name: String): Any = {
+        try {
+            val item = new Item
+            item.name(name)
+            item.save
+
+            S.notice("Item saved correctly")
+        }
+        catch  {
+            case e: Exception => {
+                S.error(e.getMessage)
+                S.redirectTo("/items/index")
+            }
+        }
+    }
+
+    private def removeItem(id: Long): () => Any = {
+        () => {
+            try {
+                val item = Item.find(id).open_!
+                item.delete_!
+
+                S.notice("Item deleted")
+            }
+            catch {
+                case e: Exception => {
+                    S.error(e.getMessage)
+                }
+            }
+        }
+    }
+
+    private def saveItem(id: Long, name: String): Any = {
+        try {
+            val item = Item.find(id).open_!
+            item.name(name)
+            item.save
+
+            S.notice("Item saved")
+        }
+        catch {
+            case e: Exception => {
+                S.error(e.getMessage)
+                S.redirectTo("/items/index")
+            }
+        }
+    }
+
+    private def getEditLink(id: Long): NodeSeq = {
+        SHtml.link("index", () => {SelectedItem(id)}, Text("edit"))
+    }
+
+    private def getRemoveLink(id: Long): NodeSeq = {
+        SHtml.link("index", removeItem(id), Text("delete"))
+    }
+}
+
